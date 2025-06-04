f073c53c37a6e6be287fe1e40eec7b29e3e589c8
coccocccoc <wod5438@naver.com>
2025-06-04 16:47:17 +0900

    추가

---
+++ react-app-1-component/src/Quiz1.js
@@ -0,0 +1,20 @@
+
+function Item() {
+  return <div>Item Component</div>;
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Item List</h1>
+      <Item></Item>
+      <Item></Item>
+      <Item></Item>
+      <Item></Item>
+      <Item></Item>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-app-1-event/src/App.js
@@ -0,0 +1,80 @@
+// WEB을 클릭하면 경고창이 나오게 이벤트 처리
+// props를 통해 onChange라는 함수가 전달됨
+function Header(props) {
+
+  console.log(props)
+
+  // a 링크는 클릭하면 다른 페이지로 이동함
+  return (
+    <header>
+      <h1><a href="/" onClick={
+        (event) => {
+          // 다른 페이지로 이동하는 기본 동작을 방지
+          event.preventDefault()
+          // WEB 링크를 클릭하면 경고창이 나타남
+          props.onChangeMode()
+        }
+      }>{ props.title }</a></h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+
+  console.log(props)
+
+  const lis = [];
+  for (let t of props.topics) {
+    // a태그에 id 추가
+    // 링크에 onclick 이벤트 추가
+    lis.push(<li key={t.id}><a id={t.id} href={"/read" + t.id} onClick={(event) => {
+
+      console.log(event.target, event.target.id)
+
+      // a 링크를 클릭하면 다른 페이지로 이동함
+      event.preventDefault() // 다른 페이지로 이동하는 기본 동작 제거
+      // props에서 전달받은 함수를 호출
+      props.onChangeMode(event.target.id);
+    } }>{ t.title }</a></li>)
+  }
+
+  return <ol>{ lis }</ol>
+  
+}
+
+function Article(props) {
+
+  return (
+    <article>
+      <h2> { props.title } </h2>
+      { props.body }
+    </article>
+  );
+}
+
+function App() {
+
+  const topics = [
+    { id: 1, title: "html", body: "html is .." },
+    { id: 2, title: "css", body: "css is .." },
+    { id: 3, title: "javascript", body: "javascript is .." }
+  ]
+
+  // 내비 목록을 클릭하면 해당 아이디가 나오도록 이벤트를 처리
+
+  return (
+    <div className="App">
+      {/* 헤더를 클릭하면 경고창이 나오는 이벤트 처리 */}
+      <Header title="WEB" onChangeMode={() => {
+        alert("Header")
+      }}></Header>
+      {/* Nav 컴포넌트에 onChangeMode라는 props 추가 */}
+      <Nav topics={topics} onChangeMode={(id) => {
+        alert(id)
+      }}></Nav>
+      <Article title="Welcome" body="Hello, React"></Article>
+    </div>
+  );
+}
+
+export default App;
