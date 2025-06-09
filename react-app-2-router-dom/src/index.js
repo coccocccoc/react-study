d3873dd9a2ec5dea89ad02bafbb3f36c59b35a97
coccocccoc <wod5438@naver.com>
2025-06-09 17:03:22 +0900

    aaa

---
+++ react-app-2-router-dom/src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// 이제 App 컴포넌트에서 라우트 기능 사용 가능!
+// Router: Url 경로에 따라 컴포넌트를 호출하는 기능
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
+
